b79a2614139a65342cf2acacf461fb45b8eb4afc
Abhaas Goyal <goyalabhaas@gmail.com>
2024-01-25 02:09:33 +1100

    Add parameterised-fixture tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 """`pytest` tests for config.py."""
-from copy import deepcopy
+from contextlib import nullcontext as does_not_raise
+from pathlib import Path
 from pprint import pformat
 
 import pytest
@@ -7,102 +8,131 @@ import pytest
 import benchcab.config as bc
 import benchcab.utils as bu
 
-no_optional_config = {
-    "project": "w97",
-    "modules": ["intel-compiler/2021.1.1", "netcdf/4.7.4", "openmpi/4.1.0"],
-    "realisations": [
-        {"repo": {"svn": {"branch_path": "trunk"}}},
-        {
-            "repo": {"svn": {"branch_path": "branches/Users/ccc561/v3.0-YP-changes"}},
+
+@pytest.fixture()
+def config_str(request) -> str:
+    """Provide relative YAML path from data files."""
+    return f"test/{request.param}"
+
+
+@pytest.fixture()
+def config_path(config_str: str) -> Path:
+    """Provide absolute YAML Path object from data files."""
+    return bu.get_installed_root() / "data" / config_str
+
+
+@pytest.fixture()
+def empty_config() -> dict:
+    """Empty dict Configuration."""
+    return {}
+
+
+@pytest.fixture()
+def no_optional_config() -> dict:
+    """Config with no optional parameters."""
+    return {
+        "project": "w97",
+        "modules": ["intel-compiler/2021.1.1", "netcdf/4.7.4", "openmpi/4.1.0"],
+        "realisations": [
+            {"repo": {"svn": {"branch_path": "trunk"}}},
+            {
+                "repo": {
+                    "svn": {"branch_path": "branches/Users/ccc561/v3.0-YP-changes"}
+                },
+            },
+        ],
+    }
+
+
+@pytest.fixture()
+def all_optional_config() -> dict:
+    """Config with all optional parameters."""
+    return {
+        "project": "w97",
+        "fluxsite": {
+            "experiment": "forty-two-site-test",
+            "multiprocess": True,
+            "pbs": {"ncpus": 18, "mem": "30GB", "walltime": "6:00:00", "storage": []},
         },
+        "modules": ["intel-compiler/2021.1.1", "netcdf/4.7.4", "openmpi/4.1.0"],
+        "realisations": [
+            {"name": None, "repo": {"svn": {"branch_path": "trunk"}}},
+            {
+                "name": None,
+                "repo": {
+                    "svn": {"branch_path": "branches/Users/ccc561/v3.0-YP-changes"}
+                },
+            },
+        ],
+        "science_configurations": [
+            {
+                "cable": {
+                    "cable_user": {"FWSOIL_SWITCH": "Haverd2013", "GS_SWITCH": "medlyn"}
+                }
+            },
+            {
+                "cable": {
+                    "cable_user": {
+                        "FWSOIL_SWITCH": "Haverd2013",
+                        "GS_SWITCH": "leuning",
+                    }
+                }
+            },
+            {
+                "cable": {
+                    "cable_user": {"FWSOIL_SWITCH": "standard", "GS_SWITCH": "medlyn"}
+                }
+            },
+            {
+                "cable": {
+                    "cable_user": {"FWSOIL_SWITCH": "standard", "GS_SWITCH": "leuning"}
+                }
+            },
+        ],
+    }
+
+
+@pytest.mark.parametrize(
+    ("config_str", "output_config", "pytest_error"),
+    [
+        ("config-valid.yml", "no_optional_config", does_not_raise()),
+        ("config-missing.yml", "empty_config", pytest.raises(FileNotFoundError)),
     ],
-}
+    indirect=["config_str"],
+)
+def test_read_config_file(config_path, output_config, pytest_error, request):
+    """Test read_config_file() for a file that may/may not exist."""
+    with pytest_error:
+        config = bc.read_config_file(config_path)
+        assert pformat(config) == pformat(request.getfixturevalue(output_config))
 
-all_optional_config = {
-    "project": "w97",
-    "fluxsite": {
-        "experiment": "forty-two-site-test",
-        "multiprocess": True,
-        "pbs": {"ncpus": 18, "mem": "30GB", "walltime": "6:00:00", "storage": []},
-    },
-    "modules": ["intel-compiler/2021.1.1", "netcdf/4.7.4", "openmpi/4.1.0"],
-    "realisations": [
-        {"name": None, "repo": {"svn": {"branch_path": "trunk"}}},
-        {
-            "name": None,
-            "repo": {"svn": {"branch_path": "branches/Users/ccc561/v3.0-YP-changes"}},
-        },
+
+@pytest.mark.parametrize(
+    ("config_str", "pytest_error"),
+    [
+        ("config-valid.yml", does_not_raise()),
+        ("config-invalid.yml", pytest.raises(bc.ConfigValidationError)),
     ],
-    "science_configurations": [
-        {
-            "cable": {
-                "cable_user": {"FWSOIL_SWITCH": "Haverd2013", "GS_SWITCH": "medlyn"}
-            }
-        },
-        {
-            "cable": {
-                "cable_user": {"FWSOIL_SWITCH": "Haverd2013", "GS_SWITCH": "leuning"}
-            }
-        },
-        {"cable": {"cable_user": {"FWSOIL_SWITCH": "standard", "GS_SWITCH": "medlyn"}}},
-        {
-            "cable": {
-                "cable_user": {"FWSOIL_SWITCH": "standard", "GS_SWITCH": "leuning"}
-            }
-        },
-    ],
-}
+    indirect=["config_str"],
+)
+def test_validate_config(config_str, pytest_error):
+    """Test validate_config() for a valid/invalid config file."""
+    with pytest_error:
+        config = bu.load_package_data(config_str)
+        assert bc.validate_config(config)
 
 
-def test_read_config_file_pass():
-    """Test read_config() reads an existing file."""
-    existent_path = bu.get_installed_root() / "data" / "test" / "config-valid.yml"
-
-    # Test for a path that exists
-    config = bc.read_config_file(existent_path)
-    assert pformat(config) == pformat(no_optional_config)
-
-
-def test_read_config_file_fail():
-    """Test that read_config() does not work for a non-existent file."""
-    nonexistent_path = bu.get_installed_root() / "data" / "test" / "config-missing.yml"
-
-    # Test for a path that does not exist.
-    with pytest.raises(FileNotFoundError):
-        _ = bc.read_config_file(nonexistent_path)
-
-
-def test_validate_config_valid():
-    """Test validate_config() for a valid config file."""
-    valid_config = bu.load_package_data("test/config-valid.yml")
-    assert bc.validate_config(valid_config)
-
-
-def test_validate_config_invalid():
-    """Test validate_config() for an invalid config file."""
-    invalid_config = bu.load_package_data("test/config-invalid.yml")
-    with pytest.raises(bc.ConfigValidationError):
-        bc.validate_config(invalid_config)
-
-
-def test_read_optional_key_add_data():
-    """Test default key-values are added if not provided by config.yaml."""
+@pytest.mark.parametrize("input_config", ["no_optional_config", "all_optional_config"])
+def test_read_optional_key_add_data(input_config, all_optional_config, request):
+    """Test default key-values are added if not provided by config.yaml, and existing keys stay intact."""
     # Config having no optional keys
-    config = deepcopy(no_optional_config)
+    config = request.getfixturevalue(input_config)
     bc.read_optional_key(config)
     assert pformat(config) == pformat(all_optional_config)
 
 
-def test_read_optional_key_same_data():
-    """Test optional key-values are unchanged if provided by config.yaml."""
-    # Config having all optional keys
-    config = deepcopy(all_optional_config)
-    bc.read_optional_key(config)
-    assert pformat(config) == pformat(all_optional_config)
-
-
-def test_read_config():
+@pytest.mark.parametrize("config_str", ["config-valid.yml"], indirect=["config_str"])
+def test_read_config(config_path, all_optional_config):
     """Test overall behaviour of read_config."""
-    expected_config = deepcopy(all_optional_config)
-    path = bu.get_installed_root() / "data" / "test" / "config-valid.yml"
-    assert pformat(bc.read_config(path)) == pformat(expected_config)
+    output_config = bc.read_config(config_path)
+    assert pformat(output_config) == pformat(all_optional_config)
